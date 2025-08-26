7a188349beecac24a5d9b1660109a76d97dfe7a7
Eduardo Giroto <eduardogiroto01@gmail.com>
2025-08-25 22:51:09 -0300

    basic sending and receiving working with docker

---
+++ Sending/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = await factory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+
+await channel.QueueDeclareAsync(queue: "hello", durable: false, exclusive: false, autoDelete: false,
+    arguments: null);
+Console.WriteLine("Type your message: ");
+while (true)
+{
+    string message = Console.ReadLine();
+    if (string.IsNullOrEmpty(message))
+        break;
+
+    var body = Encoding.UTF8.GetBytes(message);
+
+    await channel.BasicPublishAsync(exchange: string.Empty, routingKey: "hello", body: body);
+    Console.WriteLine($" [x] Sent {message}");
+
+    
+}
+
